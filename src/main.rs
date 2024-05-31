990bedf22c5518fc5e579ab9700aa08b2fc2c90f
elcio <69718786+elciod@users.noreply.github.com>
2024-05-31 02:56:19 -0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+mod read_lines; 
+mod select_boolean_in_console; 
+use passwords::PasswordGenerator;
+use read_lines::read_lines;
+use select_boolean_in_console::select_boolean_in_console;
+
+fn main() {
+    let is_length: usize = read_lines("Escreva o tamanho da password: ".to_string())
+        .trim()
+        .parse()
+        .unwrap();
+
+    let is_numbers: bool =
+        select_boolean_in_console(read_lines("Possui números? (S/N): ".to_string()));
+    let is_lowercase_letters: bool =
+        select_boolean_in_console(read_lines("Possui letras minusculas? (S/N): ".to_string()));
+    let is_uppercase_letters: bool =
+        select_boolean_in_console(read_lines("Possui letras maiusculas? (S/N): ".to_string()));
+    let is_symbols: bool =
+        select_boolean_in_console(read_lines("Possui símbolos? (S/N): ".to_string()));
+    let is_spaces: bool =
+        select_boolean_in_console(read_lines("Possui espaços? (S/N): ".to_string()));
+    let is_exclude_similar_characters: bool =
+        select_boolean_in_console(read_lines("Possui caracteres iguais? (S/N): ".to_string()));
+    let is_strict: bool = select_boolean_in_console(read_lines("Senha forte? (S/N): ".to_string()));
+
+    let password_options = PasswordGenerator {
+        length: is_length,
+        numbers: is_numbers,
+        lowercase_letters: is_lowercase_letters,
+        uppercase_letters: is_uppercase_letters,
+        symbols: is_symbols,
+        spaces: is_spaces,
+        exclude_similar_characters: is_exclude_similar_characters,
+        strict: is_strict,
+    };
+
+    let password_iterator = password_options.generate(1).unwrap();
+
+    for password in password_iterator {
+        println!("Password: {}", password)
+    }
+
+    println!("Passwords geradas com sucesso!!");
+}
---
+++ src/select_boolean_in_console.rs
@@ -0,0 +1,8 @@
+pub fn select_boolean_in_console(option: String) -> bool {
+    if option.trim() == "N" || option.trim() == "n" {
+        return false;
+    }
+
+    return true;
+    
+}
